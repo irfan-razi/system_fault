3b7e1c95289ad5fff5fc3cfd2172fdcdc4c449ae
Irfan Razi <friendship.fu@gmail.com>
2022-02-23 15:06:52 +0530

    Updated Wafer project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault",
+    version="0.0.3",
+    description="ML project",
+    author="Irfan Razi, Avnish Yadav", 
+    packages=find_packages(),
+    license="MIT"
+)
